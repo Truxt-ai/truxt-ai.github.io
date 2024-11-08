2af61f05b26290470673db222c83ad370d5e490c
prathamsaxen <er.prathamsaxena@gmail.com>
2024-11-08 17:39:17 +0530

    Added Loading & Fixed Deployment Bug

--- app/(default)/playground/page.tsx
+++ app/(default)/playground/page.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Hero from './hero';
 import IngestData from '@/components/ingest-content/ingest-data';
+import { Suspense } from 'react';
+import LoadingPage from '@/components/loading';
 
 function Page() {
-  return (
-    <div>
-        <Hero/>
-        <IngestData/>
-    </div>
-  )
+    return (
+        <div>
+            <Hero />
+            <Suspense fallback={<LoadingPage/>}>
+                <IngestData />
+            </Suspense>
+        </div>
+    );
 }
 
 export default Page;
