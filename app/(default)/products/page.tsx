4f3c74620ead346d6c9e5ee39d5160b9ee882876
prathamsaxen <er.prathamsaxena@gmail.com>
2024-11-08 18:36:44 +0530

    Products Page Enabled

--- app/(default)/products/page.tsx
+++ app/(default)/products/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Hero from '../customers/hero';
+import Hero from './hero';
+import Offerings from '@/components/offerings';
 
 function Page() {
   return (
     <div>
         <Hero/>
+        <Offerings/>
     </div>
 )
 }
