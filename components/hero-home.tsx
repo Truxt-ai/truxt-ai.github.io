0f7721ed18fd23e6fed601c3e572bc41c87a4e2f
prathamsaxen <er.prathamsaxena@gmail.com>
2024-11-10 01:59:52 +0530

    Product Page Implemented

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -32,7 +32,7 @@ export default function HeroHome() {
                                 <div className='relative mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center gap-2' data-aos='zoom-y-out' data-aos-delay={450}>
                                     <input type='email' placeholder='Business Email' className='w-1/2 rounded-lg' value={email} onChange={(e)=>setEmail(e.target.value)}/>
                                     <button className='btn group mb-4 w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto' onClick={handleClick}>
-                                        <span className='relative inline-flex items-center'>Create my playground</span>
+                                        <span className='relative inline-flex items-center'>Try it for my content</span>
                                     </button>
                                 </div>
                             </div>
