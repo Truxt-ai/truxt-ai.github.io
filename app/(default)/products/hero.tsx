cf524bb9c2e10f2597576fba8f9897cb6ac14aad
prathamsaxen <er.prathamsaxena@gmail.com>
2024-11-12 15:37:25 +0530

    Added Animation

--- app/(default)/products/hero.tsx
+++ app/(default)/products/hero.tsx
@@ -7,11 +7,11 @@ export default function HeroHome() {
             <div className='mx-auto max-w-6xl px-4 sm:px-6'>
                 <div className='pb-6 pt-32 md:pb-20 md:pt-40'>
                     <div className='pb-12 text-center md:pb-16'>
-                        <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl'>
+                        <h1 className='text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl' data-aos='zoom-y-out' data-aos-delay={300}>
                             <span className='block xl:inline'>Experience our </span> <span className='block text-primary xl:inline'>AI-Powered Products</span>
                         </h1>
                         <div className='mx-auto max-w-3xl'>
-                            <p className='mb-8 mt-2 text-lg text-gray-700' data-aos='zoom-y-out' data-aos-delay={300}>
+                            <p className='mb-8 mt-2 text-lg text-gray-700' data-aos='zoom-y-out' data-aos-delay={350}>
                                 Transform your Support requirements with our cutting-edge AI technology, our products are designed to bring greater accuracy, security and cost efficiencies.
                             </p>
                         </div>
